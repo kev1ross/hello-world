11676cfdf0e4845ce8129e8af6e8459805683540
kevin <kevin@192.168.1.80>
2018-05-15 22:40:46 +1200

    Also implemented param 3

--- src/com/kev/Hello.java
+++ src/com/kev/Hello.java
@@ -20,15 +20,20 @@ public class Hello {
 		// Initialise parameters
 		String param_1 = "";
 		String param_2 = "";
+		String param_3 = "";
 
 		//
 		if (args.length == 0) {
 			System.out.println("Arguments required");
 			System.out.println("   1: Filename");
 			System.out.println("   2: Left or Right");
+			System.out.println("   3: Up or Down");
 			System.out.println("Going to exit");
 			System.exit(0);
 		} else {
+			if (args.length > 2) {
+				param_3 = args[2];
+			}
 			if (args.length > 1) {
 				param_2 = args[1];
 			}
@@ -38,6 +43,7 @@ public class Hello {
 		}
 		System.out.println("param_1 = " + param_1);
 		System.out.println("param_2 = " + param_2);
+		System.out.println("param_3 = " + param_3);
 		
 		System.out.println("Process ended");
 	}
