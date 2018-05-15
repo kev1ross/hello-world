d14748f7e2b9559c594d55e9b74bffbd072a66a0
kevin <kevin@192.168.1.80>
2018-05-15 22:31:51 +1200

    Implemented param 1 and 2

--- src/com/kev/Hello.java
+++ src/com/kev/Hello.java
@@ -16,6 +16,29 @@ public class Hello {
 		// TODO Auto-generated method stub
 
 		System.out.println("Process started");
+		
+		// Initialise parameters
+		String param_1 = "";
+		String param_2 = "";
+
+		//
+		if (args.length == 0) {
+			System.out.println("Arguments required");
+			System.out.println("   1: Filename");
+			System.out.println("   2: Left or Right");
+			System.out.println("Going to exit");
+			System.exit(0);
+		} else {
+			if (args.length > 1) {
+				param_2 = args[1];
+			}
+			if (args.length > 0) {
+				param_1 = args[0];
+			}
+		}
+		System.out.println("param_1 = " + param_1);
+		System.out.println("param_2 = " + param_2);
+		
 		System.out.println("Process ended");
 	}
 
